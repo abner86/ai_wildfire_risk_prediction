b245acd24c39721e438773f00b2f7a21f074934f
Abner Benavides <abnerb86@gmail.com>
2023-02-22 18:08:13 -0800

    first push
    first push

---
+++ serving/data.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+import io
+import ee
+from google.api_core import exceptions, retry
+import google.auth
+import numpy as np
+from numpy.lib.recfunctions import structured_to_unstructured
+import requests
+
+SCALE = 10  # meters per pixel
+
+
+def ee_init() -> None:
+    """Authenticate and initialize Earth Engine with the default credentials."""
+    # Use the Earth Engine High Volume endpoint.
+    #   https://developers.google.com/earth-engine/cloud/highvolume
+    credentials, project = google.auth.default(
+        scopes=[
+            "https://www.googleapis.com/auth/cloud-platform",
+            "https://www.googleapis.com/auth/earthengine",
+        ]
+    )
+    ee.Initialize(
+        credentials.with_quota_project(None),
+        project=project,
+        opt_url="https://earthengine-highvolume.googleapis.com",
+    )
+
+
+def mask_sentinel2_clouds(image: ee.Image) -> ee.Image:
+    CLOUD_BIT = 10
+    CIRRUS_CLOUD_BIT = 11
+    bit_mask = (1 << CLOUD_BIT) | (1 << CIRRUS_CLOUD_BIT)
+    mask = image.select("QA60").bitwiseAnd(bit_mask).eq(0)
+    return image.updateMask(mask)
+
+
+def get_input_image(year: int, default_value: float = 1000.0) -> ee.Image:
+    landsat = (
+        ee.ImageCollection("COPERNICUS/S2_HARMONIZED")
+        .filterDate(f"{year}-1-1", f"{year}-12-31")
+        .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE", 20))
+        .map(mask_sentinel2_clouds)
+        .select("B.*")
+        .median()
+        .unmask(default_value)
+    )
+    # modis = ee.ImageCollection("MODIS/061/MCD43A4").filterDate(f"{year}-1-1", f"{year}-12-31").select("Nadir_Reflectance_B.*").median()
+    firms_collection = (
+        ee.ImageCollection("FIRMS")
+        .filterDate(f"{year}-1-1", f"{year}-12-31")
+        .select("T21")
+        .median()
+    )
+    env_collection = (
+        ee.ImageCollection("IDAHO_EPSCOR/GRIDMET")
+        .filterDate(f"{year}-1-1", f"{year}-12-31")
+        .select(["tmmx", "tmmn", "pr", "vs", "sph"])
+        .median()
+    )
+    veg_collection = (
+        ee.ImageCollection("MODIS/006/MOD13A2")
+        .filterDate(f"{year}-1-1", f"{year}-12-31")
+        .select("NDVI")
+        .median()
+    )
+    elevation = ee.Image("CGIAR/SRTM90_V4").select("elevation")
+    merged_image = (
+        landsat.addBands(firms_collection)
+        .addBands(env_collection)
+        .addBands(veg_collection)
+        .addBands(elevation)
+    )
+    return merged_image.unmask(0).float()
+
+
+def get_label_image() -> ee.Image:
+    label_asset_id = "projects/wildfire-risk-prediction/assets/whp_2020"
+    # Load the image from the asset
+    return ee.Image(label_asset_id)
+
+
+def get_input_patch(year: int, lonlat, patch_size: int) -> np.ndarray:
+    image = get_input_image(year)
+    patch = get_patch(image, lonlat, patch_size, SCALE)
+    return structured_to_unstructured(patch)
+
+
+def get_label_patch(lonlat, patch_size: int) -> np.ndarray:
+    image = get_label_image()
+    patch = get_patch(image, lonlat, patch_size, SCALE)
+    return structured_to_unstructured(patch)
+
+
+@retry.Retry(deadline=10 * 60)  # seconds
+def get_patch(image: ee.Image, lonlat, patch_size: int, scale: int) -> np.ndarray:
+    point = ee.Geometry.Point(lonlat)
+    url = image.getDownloadURL(
+        {
+            "region": point.buffer(scale * patch_size / 2, 1).bounds(1),
+            "dimensions": [patch_size, patch_size],
+            "format": "NPY",
+        }
+    )
+    # If we get "429: Too Many Requests" errors, it's safe to retry the request.
+    # The Retry library only works with `google.api_core` exceptions.
+    response = requests.get(url)
+    if response.status_code == 429:
+        raise exceptions.TooManyRequests(response.text)
+
+    # Still raise any other exceptions to make sure we got valid data.
+    response.raise_for_status()
+    return np.load(io.BytesIO(response.content), allow_pickle=True)
+
---
+++ visualize.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+import numpy as np
+from plotly.graph_objects import Image
+from plotly.subplots import make_subplots
+
+CLASSIFICATIONS = {
+    "🪨 No Data": "FFFFFF",
+    "🟢 Very Low": "38A800",
+    "🍏 Low": "D1FF73",
+    "🟡 Moderate": "FFFF00",
+    "🟠 High": "FFAA00",
+    "🔴 Very High": "FF0000",
+    "⚫ Non-burnable": "B2B2B2",
+    "💧 Water": "0070FF",
+}
+
+
+def render_classifications(values: np.ndarray, palette):
+    """Renders a classifications NumPy array with shape (width, height, 1) as an image.
+    Args:
+        values: An uint8 array with shape (width, height, 1).
+        palette: List of hex encoded colors.
+    Returns: An uint8 array with shape (width, height, rgb) with colors from the palette.
+    """
+    # Create a color map from a hex color palette.
+    xs = np.linspace(0, len(palette), 256)
+    indices = np.arange(len(palette))
+
+    red = np.interp(xs, indices, [int(c[0:2], 16) for c in palette])
+    green = np.interp(xs, indices, [int(c[2:4], 16) for c in palette])
+    blue = np.interp(xs, indices, [int(c[4:6], 16) for c in palette])
+
+    color_map = np.array([red, green, blue]).astype(np.uint8).transpose()
+    color_indices = (values / len(palette) * 255).astype(np.uint8)
+    return np.take(color_map, color_indices, axis=0)
+
+
+def render_rgb_images(
+    values: np.ndarray, min: float = 0.0, max: float = 1.0
+) -> np.ndarray:
+    """Renders a numeric NumPy array with shape (width, height, rgb) as an image.
+    Args:
+        values: A float array with shape (width, height, rgb).
+        min: Minimum value in the values.
+        max: Maximum value in the values.
+    Returns: An uint8 array with shape (width, height, rgb).
+    """
+    scaled_values = (values - min) / (max - min)
+    rgb_values = np.clip(scaled_values, 0, 1) * 255
+    return rgb_values.astype(np.uint8)
+
+
+def render_label_image(patch: np.ndarray):
+    """Renders a land cover image."""
+    palette = list(CLASSIFICATIONS.values())
+    return render_classifications(patch[:, :, 0], palette)
+
+
+def render_input(patch: np.ndarray, max: float = 3000) -> np.ndarray:
+    red = patch[:, :, 3]
+    green = patch[:, :, 2]
+    blue = patch[:, :, 1]
+    rgb_patch = np.stack([red, green, blue], axis=-1)
+    return render_rgb_images(rgb_patch, 0, max)
+
+
+def show_example(inputs: np.ndarray, labels: np.ndarray, max: float = 3000):
+    """Shows an example of inputs and labels an image."""
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Input", "WHP"))
+    fig.add_trace(Image(z=render_input(inputs, max)), row=1, col=1)
+    fig.add_trace(Image(z=render_label_image(labels)), row=1, col=2)
+    fig.show()
+
+
+def show_legend() -> None:
+    """Shows the legend of the land cover classifications."""
+
+    def color_box(red: int, green: int, blue: int) -> str:
+        return f"\033[48;2;{red};{green};{blue}m"
+
+    reset_color = "\u001b[0m"
+    for name, color in CLASSIFICATIONS.items():
+        red = int(color[0:2], 16)
+        green = int(color[2:4], 16)
+        blue = int(color[4:6], 16)
+        print(f"{color_box(red, green, blue)}   {reset_color} {name}")
